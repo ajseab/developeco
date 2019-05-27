ab6c8b60964c75f9e59892281c151f665aa6f0d6
ajseab <averyjseabolt@gmail.com>
2019-05-26 20:41:04 -0700

    stylized designs page. created a responsive flexbox grid layout

--- js/script.js
+++ js/script.js
@@ -1,41 +1,10 @@
 console.log("hello world");
 
-const igSources = [
-    "https://www.instagram.com/p/BtHcxr3AvgW/",
-    "https://www.instagram.com/p/Bs9zlOQg90-/",
-    "https://www.instagram.com/p/BsrkZXKgMij/",
-    "https://www.instagram.com/p/BspW7c-A6O9/",
-    "https://www.instagram.com/p/BsWFMInAIRc/",
-    "https://www.instagram.com/p/BsSYK6hgd_e/"
-];
+// const designImgNodes = document.querySelectorAll(".design--img");
+// for (let i = 0; i < designImgNodes.length; i++) {
+//   designImgNodes[i].addEventListener("click", function () {
+//     console.log("your mom gay");
+//     designImgNodes[i].style = "width:600px;height:600px;position:absolute;";
 
-// function createIgFeed(urlLi) {
-//     for (let i = 0; i < urlLi.length; i++) {
-//         let source = urlLi[i].substr(28,11);
-//         let title = $.getJSON(`https://api.instagram.com/oembed?url=http://instagr.am/p/${source}/`, function(postData) {
-//             console.log(postData.title);
-//         });
-//         let img = `<div><img src="${urlLi[i]}media/" alt=""><p>${title}</p></div>`;
-//         let imgContainer = document.getElementById("instagramFeed");
-//         imgContainer.innerHTML += img;
-//     }
+//   })
 // }
-
-function createIgFeed(urlLi) {
-    for (let i = 0; i < urlLi.length; i++) {
-        let source = urlLi[i].substr(28,11);
-        fetch(`https://api.instagram.com/oembed?url=http://instagr.am/p/${source}/`).then(response => {
-          return response.json();
-        }).then(data => {
-          // Work with JSON data here
-          const img = `<div><img src="${urlLi[i]}media/" alt=""><p>${data.title}</p></div>`;
-          let imgContainer = document.getElementById("instagramFeed");
-          imgContainer.innerHTML += img;
-          console.log(data.title);
-        }).catch(err => {
-          // Do something for an error here
-        });
-    }
-}
-
-createIgFeed(igSources);
